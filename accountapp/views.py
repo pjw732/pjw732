f1da1fd81355431cf5c5279d3ce07c235f9ebd14
pjw732 <86655858+pjw732@users.noreply.github.com>
2021-07-12 11:42:42 +0900

    07.12_2

--- accountapp/views.py
+++ accountapp/views.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+from accountapp.models import HelloWorld
 
 
 def hello_world(request):
@@ -9,8 +10,12 @@ def hello_world(request):
 
         temp = request.POST.get('input_text')
 
+        new_hello_world = HelloWorld()
+        new_hello_world.text = temp
+        new_hello_world.save()
+
         return render(request, 'accountapp/hello_world.html',
-                      context={'text': temp})     # alt+enter 이름이 같으면 모듈 가져옴
+                      context={'new_hello_world': new_hello_world})     # alt+enter 이름이 같으면 모듈 가져옴
     else:
         return render(request, 'accountapp/hello_world.html',
                       context={'text': 'POST METHOD!'})
