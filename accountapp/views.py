7caaeb5dcb1563c940cdf67b2a59234e18f90953
pjw732 <86655858+pjw732@users.noreply.github.com>
2021-07-12 11:14:01 +0900

    07.12_1

--- accountapp/views.py
+++ accountapp/views.py
@@ -6,8 +6,11 @@ from django.shortcuts import render
 
 def hello_world(request):
     if request.method == "POST":
+
+        temp = request.POST.get('input_text')
+
         return render(request, 'accountapp/hello_world.html',
-                      context={'text': 'POST METHOD!'})     # alt+enter 이름이 같으면 모듈 가져옴
+                      context={'text': temp})     # alt+enter 이름이 같으면 모듈 가져옴
     else:
         return render(request, 'accountapp/hello_world.html',
-                      context={'text': 'GET METHOD!'})
+                      context={'text': 'POST METHOD!'})
