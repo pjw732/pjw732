99fa1abd9c2c636d3023d5e4a8ca9e5303d71586
pjw732 <86655858+pjw732@users.noreply.github.com>
2021-07-26 11:43:07 +0900

    07.26_1

--- profileapp/models.py
+++ profileapp/models.py
@@ -1,3 +1,12 @@
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE,
+                                related_name='profile')
+
+    image = models.ImageField(upload_to='profile/', null=True)
+    nickname = models.CharField(max_length=30, unique=True, null=True)      # CharField 사용 시 글자수 설정
+    message = models.CharField(max_length=200, null=True)
