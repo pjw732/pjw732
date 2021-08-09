5276c052822a3f1d822b2b405f53533e3b81dabc
pjw732 <86655858+pjw732@users.noreply.github.com>
2021-08-09 11:28:53 +0900

    08.09_1

--- articleapp/decorators.py
+++ articleapp/decorators.py
@@ -5,7 +5,7 @@ from articleapp.models import Article
 
 def article_ownership_required(func):
     def decorated(request, *args, **kwargs):
-        target_article = Article.objexts.get(pk=kwargs['pk'])
+        target_article = Article.objects.get(pk=kwargs['pk'])
         if target_article.writer == request.user:
             return func(request, *args, **kwargs)
         else:
