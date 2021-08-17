82215d186d0acf4befc46add74b1770ee252d8eb
pjw732 <86655858+pjw732@users.noreply.github.com>
2021-08-17 18:14:54 +0900

    08.010

--- practice01/urls.py
+++ practice01/urls.py
@@ -24,5 +24,6 @@ urlpatterns = [
     path('profiles/', include('profileapp.urls')),
     path('articles/', include('articleapp.urls')),
     path('comment/', include('commentapp.urls')),
+    path('project/', include('projectapp.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
