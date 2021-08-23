e4e0cdab81a63ee4a78a850e5909802469487789
pjw732 <86655858+pjw732@users.noreply.github.com>
2021-08-23 11:27:42 +0900

    08.23_1

--- practice01/urls.py
+++ practice01/urls.py
@@ -25,5 +25,6 @@ urlpatterns = [
     path('articles/', include('articleapp.urls')),
     path('comment/', include('commentapp.urls')),
     path('project/', include('projectapp.urls')),
+    path('subscribe/', include('subscribeapp.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
